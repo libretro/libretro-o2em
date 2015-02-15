f9cb25b78a40efadcc192fe03012b9313d62f73c
twinaphex <libretro@gmail.com>
2015-02-15 23:12:54 +0100

    Remove screenshot code in keyboard.c

--- src/keyboard.c
+++ src/keyboard.c
@@ -28,12 +28,8 @@
 #include "vpp.h"
 #include "keyboard.h"
 
-#ifndef __LIBRETRO__
-#include "allegro.h"
-#else
 #include "libretro.h"
 #include "wrapalleg.h"
-#endif
 
 #include "score.h"
 
@@ -45,123 +41,7 @@ Byte new_int=0;	/* Is new interrupt installed */
 
 Byte key_done=0;
 Byte key_debug=0;
-#ifndef __LIBRETRO__
 
-struct keyb keybtab[] = {
-	{KEY_A,"A"},
-	{KEY_B,"B"},
-	{KEY_C,"C"},
-	{KEY_D,"D"},
-	{KEY_E,"E"},
-	{KEY_F,"F"},
-	{KEY_G,"G"},
-	{KEY_H,"H"},
-	{KEY_I,"I"},
-	{KEY_J,"J"},
-	{KEY_K,"K"},
-	{KEY_L,"L"},
-	{KEY_M,"M"},
-	{KEY_N,"N"},
-	{KEY_O,"O"},
-	{KEY_P,"P"},
-	{KEY_Q,"Q"},
-	{KEY_R,"R"},
-	{KEY_S,"S"},
-	{KEY_T,"T"},
-	{KEY_U,"U"},
-	{KEY_V,"V"},
-	{KEY_W,"W"},
-	{KEY_X,"X"},
-	{KEY_Y,"Y"},
-	{KEY_Z,"Z"},
-	{KEY_0,"0"},
-	{KEY_1,"1"},
-	{KEY_2,"2"},
-	{KEY_3,"3"},
-	{KEY_4,"4"},
-	{KEY_5,"5"},
-	{KEY_6,"6"},
-	{KEY_7,"7"},
-	{KEY_8,"8"},
-	{KEY_9,"9"},
-	{KEY_0_PAD,"0_PAD"},
-	{KEY_1_PAD,"1_PAD"},
-	{KEY_2_PAD,"2_PAD"},
-	{KEY_3_PAD,"3_PAD"},
-	{KEY_4_PAD,"4_PAD"},
-	{KEY_5_PAD,"5_PAD"},
-	{KEY_6_PAD,"6_PAD"},
-	{KEY_7_PAD,"7_PAD"},
-	{KEY_8_PAD,"8_PAD"},
-	{KEY_9_PAD,"9_PAD"},
-	{KEY_TILDE,"TILDE"},
-	{KEY_MINUS,"MINUS"},
-	{KEY_EQUALS,"EQUALS"},
-	{KEY_BACKSPACE,"BACKSPACE"},
-	{KEY_TAB,"TAB"},
-	{KEY_OPENBRACE,"OPENBRACE"},
-	{KEY_CLOSEBRACE,"CLOSEBRACE"},
-	{KEY_ENTER,"ENTER"},
-	{KEY_COLON,"COLON"},
-	{KEY_QUOTE,"QUOTE"},
-	{KEY_BACKSLASH,"BACKSLASH"},
-	{KEY_BACKSLASH2,"BACKSLASH2"},
-	{KEY_COMMA,"COMMA"},
-	{KEY_STOP,"STOP"},
-	{KEY_SLASH,"SLASH"},
-	{KEY_SPACE,"SPACE"},
-	{KEY_INSERT,"INSERT"},
-	{KEY_DEL,"DEL"},
-	{KEY_HOME,"HOME"},
-	{KEY_END,"END"},
-	{KEY_PGUP,"PGUP"},
-	{KEY_PGDN,"PGDN"},
-	{KEY_LEFT,"LEFT"},
-	{KEY_RIGHT,"RIGHT"},
-	{KEY_UP,"UP"},
-	{KEY_DOWN,"DOWN"},
-	{KEY_SLASH_PAD,"SLASH_PAD"},
-	{KEY_ASTERISK,"ASTERISK"},
-	{KEY_MINUS_PAD,"MINUS_PAD"},
-	{KEY_PLUS_PAD,"PLUS_PAD"},
-	{KEY_DEL_PAD,"DEL_PAD"},
-	{KEY_ENTER_PAD,"ENTER_PAD"},
-	{KEY_PRTSCR,"PRTSCR"},
-	{KEY_PAUSE,"PAUSE"},
-	{KEY_ABNT_C1,"ABNT_C1"},
-	{KEY_YEN,"YEN"},
-	{KEY_KANA,"KANA"},
-	{KEY_AT,"AT"},
-	{KEY_CIRCUMFLEX,"CIRCUMFLEX"},
-	{KEY_COLON2,"COLON2"},
-	{KEY_KANJI,"KANJI"},
-	{KEY_LSHIFT,"LSHIFT"},
-	{KEY_RSHIFT,"RSHIFT"},
-	{KEY_LCONTROL,"LCONTROL"},
-	{KEY_RCONTROL,"RCONTROL"},
-	{KEY_ALT,"ALT"},
-	{KEY_ALTGR,"ALTGR"},
-	{KEY_LWIN,"LWIN"},
-	{KEY_RWIN,"RWIN"},
-	{KEY_MENU,"MENU"},
-	{KEY_SCRLOCK,"SCRLOCK"},
-	{KEY_NUMLOCK,"NUMLOCK"},
-	{KEY_F1,"F1"},
-	{KEY_F2,"F2"},
-	{KEY_F3,"F3"},
-	{KEY_F4,"F4"},
-	{KEY_F5,"F5"},
-	{KEY_F6,"F6"},
-	{KEY_F7,"F7"},
-	{KEY_F8,"F8"},
-	{KEY_F9,"F9"},
-	{KEY_F10,"F10"},
-	{KEY_F11,"F11"},
-	{KEY_F12,"F12"},
-	{KEY_ESC,"ESC"},
-    {0,""}
-};
-#else
 #define RETROK_TILDE 178
 
 struct keyb keybtab[] = {
@@ -278,7 +158,6 @@ struct keyb keybtab[] = {
 	{RETROK_ESCAPE,"ESC"},
     {0,""}
 };
-#endif
 
 int joykeys[2][5] = {{0,0,0,0,0},{0,0,0,0,0}};
 int joykeystab[128];
@@ -483,38 +362,6 @@ void handle_key(void){
 	{
 		set_score(app_data.scoretype, app_data.scoreaddress, app_data.default_highscore);
 	}
-
- 
-	if (key[syskeys[4]]) {
-#ifndef __LIBRETRO__
-		BITMAP *bmp;
-		PALETTE pal;
-		char *p;
-		static char name[1024];
-		static int scshot_counter = 0;
-
-		if (strlen(app_data.scshot)>0){
-			if ((p=strchr(app_data.scshot,'@'))) {
-				*p = 0;
-				sprintf(name, "%s%02d%s", app_data.scshot, scshot_counter++, p+1);
-				*p = '@';
-			} else {
-				strcpy(name, app_data.scshot);
-			}
-			get_palette(pal);
-			bmp = create_sub_bitmap(screen, 0, 0, SCREEN_W, SCREEN_H);
-			save_bitmap(name, bmp, pal);
-			destroy_bitmap(bmp);
-			do {
-				rest(5);
-				if (NeedsPoll) 
-				poll_keyboard();
-
-			} while (key[syskeys[4]]);
-		}
-#endif
-	}
-
 
 	if (key[RETROK_LALT] && key[RETROK_RETURN]) {
 		app_data.fullscreen = app_data.fullscreen ? 0 : 1;

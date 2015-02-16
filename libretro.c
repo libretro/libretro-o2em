2c0bb6ca443febc2a80bcbbb5557361616c7f959
twinaphex <libretro@gmail.com>
2015-02-16 04:21:05 +0100

    Set need_fullpath to true for now until we no longer need to do file I/O for ROM reading

--- libretro.c
+++ libretro.c
@@ -368,7 +368,7 @@ void retro_get_system_info(struct retro_system_info *info)
    memset(info, 0, sizeof(*info));
 	info->library_name = "O2EM";
 	info->library_version = "1.18";
-	info->need_fullpath = false;
+	info->need_fullpath = true;
 	info->valid_extensions = "bin";
 }
 

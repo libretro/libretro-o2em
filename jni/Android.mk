6d3666379c89feaafebeb06696d23538ea555919
Aaron Kling <webgeek1234@gmail.com>
2018-04-09 10:12:42 -0500

    Refactor android jni makefiles

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,38 +1,24 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-
-GIT_VERSION := " $(shell git rev-parse --short HEAD || echo unknown)"
-ifneq ($(GIT_VERSION)," unknown")
-	LOCAL_CFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
-endif
-
-LOCAL_MODULE    := retro
-
-ifeq ($(TARGET_ARCH),arm)
-LOCAL_CFLAGS += -DANDROID_ARM
-LOCAL_ARM_MODE := arm
-ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-LOCAL_CFLAGS += -D__ARM_NEON__ -mfpu=neon
-endif
-endif
-
-ifeq ($(TARGET_ARCH),x86)
-LOCAL_CFLAGS +=  -DANDROID_X86
-endif
-
-ifeq ($(TARGET_ARCH),mips)
-LOCAL_CFLAGS += -DANDROID_MIPS -D__mips__ -D__MIPSEL__
-endif
-
-CORE_DIR := ..
+CORE_DIR := $(LOCAL_PATH)/..
 
 include $(CORE_DIR)/Makefile.common
 
-LOCAL_SRC_FILES    += $(SOURCES_C)
+COREFLAGS := -D__LIBRETRO__ -DANDROID $(INCFLAGS)
 
-LOCAL_C_INCLUDES = $(INCFLAGS)
+GIT_VERSION := " $(shell git rev-parse --short HEAD || echo unknown)"
+ifneq ($(GIT_VERSION)," unknown")
+  COREFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
+endif
 
-LOCAL_CFLAGS += -O3 -std=gnu99 -D__LIBRETRO__ -DFRONTEND_SUPPORTS_RGB565 -DANDROID $(INCFLAGS)
+include $(CLEAR_VARS)
+LOCAL_MODULE    := retro
+LOCAL_SRC_FILES := $(SOURCES_C)
+LOCAL_CFLAGS    := -std=gnu99 $(COREFLAGS)
+LOCAL_LDFLAGS   := -Wl,-version-script=$(CORE_DIR)/link.T
+
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+  LOCAL_ARM_NEON := true
+endif
 
 include $(BUILD_SHARED_LIBRARY)
